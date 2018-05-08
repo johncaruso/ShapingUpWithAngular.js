012800e8b2c914a818bca5ff74283c4d005da1b0
John Caruso <johncaruso@email.com>
2018-05-07 20:34:17 -0500

    Parse Table API response into expected objects

--- sn/sp_widget/store.js
+++ sn/sp_widget/store.js
@@ -2,82 +2,26 @@ function store($http) {
     /* widget controller */
     var c = this;
 
-    c.products = [{
-        name: 'Azurite',
-        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-        shine: 8,
-        price: 110.50,
-        rarity: 7,
-        color: '#CCC',
-        faces: 14,
-        images: [
-            "x_9210_suwa_img_gem-02.gif",
-            "x_9210_suwa_img_gem-05.gif",
-            "x_9210_suwa_img_gem-09.gif"
-        ],
-        reviews: [{
-            stars: 5,
-            body: "I love this gem!",
-            author: "joe@thomas.com",
-            createdOn: 1397490980837
-        }, {
-            stars: 1,
-            body: "This gem sucks.",
-            author: "tim@hater.com",
-            createdOn: 1397490980837
-        }]
-    }, {
-        name: 'Bloodstone',
-        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-        shine: 9,
-        price: 22.90,
-        rarity: 6,
-        color: '#EEE',
-        faces: 12,
-        images: [
-            "x_9210_suwa_img_gem-01.gif",
-            "x_9210_suwa_img_gem-03.gif",
-            "x_9210_suwa_img_gem-04.gif"
-        ],
-        reviews: [{
-            stars: 3,
-            body: "I think this gem was just OK, could honestly use more shine, IMO.",
-            author: "JimmyDean@sausage.com",
-            createdOn: 1397490980837
-        }, {
-            stars: 4,
-            body: "Any gem with 12 faces is for me!",
-            author: "gemsRock@alyssaNicoll.com",
-            createdOn: 1397490980837
-        }]
-    }, {
-        name: 'Zircon',
-        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-        shine: 70,
-        price: 1100,
-        rarity: 2,
-        color: '#000',
-        faces: 6,
-        images: [
-            "x_9210_suwa_img_gem-06.gif",
-            "x_9210_suwa_img_gem-07.gif",
-            "x_9210_suwa_img_gem-10.gif"
-        ],
-        reviews: [{
-            stars: 1,
-            body: "This gem is WAY too expensive for its rarity value.",
-            author: "turtleguyy@zdn.me",
-            createdOn: 1397490980837
-        }, {
-            stars: 1,
-            body: "BBW: High Shine != High Quality.",
-            author: "LouisW407@gmail.com",
-            createdOn: 1397490980837
-        }, {
-            stars: 1,
-            body: "Don't waste your rubles!",
-            author: "nat@flatland.com",
-            createdOn: 1397490980837
-        }]
-    }];
+    c.products = [];
+
+    $http.get('/api/now/table/x_9210_suwa_product', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-UserToken': window.g_ck
+        },
+        params: {
+            'sysparm_display_value': true
+        }
+    }).then(function (response) {
+        c.products = response.data.result.map(function (product) {
+            product.images = product.images.split(', ');
+            product.shine = parseInt(product.shine, 10);
+            product.rarity = parseInt(product.rarity, 10);
+            product.faces = parseInt(product.faces, 10);
+            product.price = parseFloat(product.price.substring(1));
+            return product;
+        });
+        console.log(c.products);
+    });
 }
